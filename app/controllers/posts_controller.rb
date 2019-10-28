7d21f0684ad164b6db4a50e96078d7a2cb823207
oobobeo <gkstmdok@yonsei.ac.kr>
2019-10-28 18:24:54 +0900

    post_controller.create creates Post row, but has no column :title, :body

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,25 @@ class PostsController < ApplicationController
 
 	end
 
+	def show
+		@post = Post.find(params[:id])
+		puts '--------------'
+		puts @post.inspect
+	end
+
 	def new
 
 	end
 
 	def create
-		render plain: params[:post].inspect
+		# render plain: params[:post].inspect
+		@post = Post.new(post_params)
+
+		@post.save
+		redirect_to @post
+	end
+
+	private def post_params
+		params.require(:post).permit(:title, :body)
 	end
 end
---
+++ app/models/post.rb
@@ -0,0 +1,3 @@
+class Post < ApplicationRecord
+	attr_accessor :title, :body
+end
